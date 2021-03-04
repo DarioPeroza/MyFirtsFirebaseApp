5cec69386db6979d17fc1f4175d63666d84469c5
Dario Peroza <exskey@gmail.com>
2021-03-04 16:10:46 -0400

    Add login process

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const singUpForm    = initUser[1];                              //Select form of
 
 //---------------------------------------------Functions---------------------------------------------
 
-function hideModal(modalId) {
+function hideModal(modalId) {                   //function for hide modal with id
 
     if (modalId[0] != "#") {
 
@@ -18,7 +18,7 @@ function hideModal(modalId) {
 
 }
 
-function captureUserAuthData(form) {
+function captureUserAuthData(form) {            //capture user credential of form
 
     let userAuthData            = {};
         userAuthData.email      = form[0].value;
@@ -28,7 +28,7 @@ function captureUserAuthData(form) {
 
 }
 
-function registerNewUser(){
+function registerNewUser(){                     //Register function
 
     let user = captureUserAuthData(singUpForm);
     let modalId = "singUpModal";
@@ -49,8 +49,24 @@ function registerNewUser(){
 
 }
 
-function getRegisterUser(email, password) {
-    console.log("Hello old user");
+function initSession() {                        //Start session
+
+    let user = captureUserAuthData(singInForm);
+    let modalId = "singInModal";
+    
+    auth
+        .signInWithEmailAndPassword(user.email, user.password)
+        .then(userCredential => {
+            console.log(userCredential);
+        })
+        .then(() => {
+            alert("Success!");
+            singInForm.reset();
+            hideModal(modalId);
+        }, () => {
+            alert("Invalid email or password");
+            singInForm.reset();
+        })
 }
 
 //-----------------------------------------------Events-----------------------------------------------
@@ -62,5 +78,5 @@ singUpForm.addEventListener("submit", (e) => {
 
 singInForm.addEventListener("submit", (e) => {
     e.preventDefault();
-    getRegisterUser();
+    initSession();
 });

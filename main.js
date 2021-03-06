953d9ebda57d90e297ca6244eca9791de7982b29
Dario Peroza <exskey@gmail.com>
2021-03-06 15:16:00 -0400

    Add identify with Google

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
 // Need loginFunctions.js & simpleFunctions.js to work
+
 //---------------------------------------------Vars---------------------------------------------
-const initUser          = document.querySelectorAll(".initUser");       //Select forms
-const singInForm        = initUser[0];                                  //Select form of singIn
-const singUpForm        = initUser[1];                                  //Select form of singUp
-const logoutButton      = document.querySelector("#logoutButton");      //Select logout Button
-const navLinks          = document.querySelectorAll(".nav-link");       //Select all nav-link
-const singInNavLink     = navLinks[0];
-const singUpNavLink     = navLinks[1];
-const logoutNavLink     = navLinks[2];
-const postsContainer    = document.getElementById("allPostContainer");
+
+// Forms vars
+const initUser              = document.querySelectorAll(".initUser");
+const singInForm            = initUser[0];
+const singUpForm            = initUser[1];
+// Modal buttons
+const logoutButton          = document.querySelector("#logoutButton");
+const navLinks              = document.querySelectorAll(".nav-link");
+const singInNavLink         = navLinks[0];
+const singUpNavLink         = navLinks[1];
+const logoutNavLink         = navLinks[2];
+const googleButtons         = document.getElementsByClassName("google-buttons");
+const googleSingInButton    = googleButtons[0];
+const googleSingUpButton    = googleButtons[1];
+const facebookButtons       = document.getElementsByClassName("facebook-buttons");
+const facebookSingInButton  = facebookButtons[0];
+const facebookSingUpButton  = facebookButtons[1];
+// Posts container
+const postsContainer        = document.getElementById("allPostContainer");
 
 //---------------------------------------------Functions---------------------------------------------
 
@@ -19,8 +30,8 @@ function writePosts(snapshot) {
         let article = `
             <li class="list-group-item list-group-item-action">
                 <h3>${post.title}</h3>
-                <section>${post.body}</section>
-                <p>${post.author}</p>
+                <p>${post.body}</p>
+                <h6>${post.author}</h6>
             </li>
         `;
         write += article;
@@ -30,7 +41,7 @@ function writePosts(snapshot) {
 
 //-----------------------------------------------Events-----------------------------------------------
 
-// Buttons
+// Buttons events
 singUpForm.addEventListener("submit", (e) => {
     e.preventDefault();
     registerNewUser();
@@ -48,6 +59,20 @@ logoutButton.addEventListener("click", (e) => {
         switchNavLinks();
         })
 })
+
+googleSingInButton.addEventListener("click", e => {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider)
+        .then(result => {
+            console.log(result);
+        })
+});
+
+//googleSingUpButton.addEventListener("click", );
+
+//facebookSingInButton.addEventListener("click", );
+
+//facebookSingUpButton.addEventListener("click", );
 
 // auth event...
 auth.onAuthStateChanged((user) => {
